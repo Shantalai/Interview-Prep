40f9391418cac5563175293897686b3657c5f617
Anastasia <omkalimata09@gmail.com>
2021-01-04 11:48:29 -0500

    Update easy.js

--- easy.js
+++ easy.js
@@ -177,6 +177,23 @@ var reverseList = function(head) {
     return head;
 };
 
+//237. Delete Node in a Linked List
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val) {
+ *     this.val = val;
+ *     this.next = null;
+ * }
+ */
+/**
+ * @param {ListNode} node
+ * @return {void} Do not return anything, modify node in-place instead.
+ */
+var deleteNode = function(node) {
+    node.val = node.next.val;
+    node.next = node.next.next;
+};
+
 //104. Maximum Depth of Binary Tree
 /**
  * Definition for a binary tree node.
